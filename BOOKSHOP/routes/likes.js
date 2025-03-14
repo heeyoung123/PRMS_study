00a82454af6bb6e7e96a97a187636eb5f48b93c0
heeyoung123 <heeyoung321@naver.com>
2025-03-14 21:47:35 +0900

    8 · 파트 05

--- BOOKSHOP/routes/likes.js
+++ BOOKSHOP/routes/likes.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const {addLike, removeLike} = require("../controller/likeController");
 
 router.use(express.json());
 //좋아요 추가
-router.post("/:id", (req, res) => {
-});
+router.post("/:id", addLike);
 //좋아요 취소
-router.delete("/:id", (req, res) => {
-});
+router.delete("/:id", removeLike);
 
 
 module.exports = router;
